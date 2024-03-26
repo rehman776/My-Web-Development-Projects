c22488c192155c8af644c967eae811dff6e8c643
Abdul Rehman <121892784+rehman776@users.noreply.github.com>
2024-03-26 12:29:52 -0700

    Add files via upload

---
+++ My JavaScript Classes/Js_tut02.js
@@ -0,0 +1,3 @@
+// In this file we add Js in external file and we link this file "Js_tut02.html" 
+
+document.write("Js In External File");
---
+++ My JavaScript Classes/functions.js
@@ -0,0 +1,55 @@
+// simple js variable.
+
+// var a = 15;
+// var b = 13;
+// var c = a + b;
+// console.log(c);
+
+// simple function.
+
+// function add(a, b) {
+//     var c = a + b;
+//     console.log(c);
+// }
+// add(15, 18);
+
+// simple function with return.
+
+//  function add() {
+//     document.write('Hello World');
+//  }
+
+// function addNumber(a, b, c) {
+//     var d = a + b + c;
+//     console.log(d);
+// }
+// addNumber(15, 18, 20);
+
+// function myIntro() {
+//     console.log('Hey my name is Abdul Rehman.')
+//     console.log('My father name is M.Ramzan.')
+//     console.log('I am from Kasur Village Nizqm Pura.')
+//     console.log('I am a front-end developer.')
+//     console.log('Now i am started learning JavaScript.')
+// }
+// myIntro();
+
+// function with out parametar
+
+// function myIntro() {
+//     console.log('Hey my name is Ahmad.')
+//     console.log('My father name is Nawaz.')
+//     console.log('I am from Kasur Village Nizqm Pura.')
+//     console.log('I am a front-end developer.')
+//     console.log('Now i am started learning JavaScript.')
+// }
+// myIntro();
+
+// function with parametar
+
+function fullName(fname, lname) {
+    console.log('My full name is ' + fname + ' ' + lname);
+    console.log('My father full name is ' + fname + ' ' + lname);
+}
+
+fullName('Abdul', 'Rehman');
